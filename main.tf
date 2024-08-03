5cc2c823de7f48163735326fbd0be8aa7af734bc
Ajithkumar R <168090251+arajendran18@users.noreply.github.com>
2024-08-03 22:52:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,28 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket  = "my-unique-bucket-name18961234"
-  tags    = {
-        Name          = "MyS3Bucket"
-        Environment    = "Production"
-  }
+resource "aws_instance" "ec2_example" {
+    ami = "ami-0c2af51e265bd5e0e"
+    instance_type = var.instance_type
+    count = var.instance_count
+    associate_public_ip_address = var.enable_public_ip
+    tags = {
+        Name = "Terraform EC2"
+    }
 }
+variable "instance_type" {
+   description = "Instance type t2.micro"
+   type        = string
+   default     = "t2.micro"
+}
+variable "instance_count" {
+   description = "Instance type t2.micro"
+   type        = number
+   default     = 2
+}
+variable "enable_public_ip" {
+  description = "Enable public IP address"
+  type        = bool
+  default     = true
+}
+
