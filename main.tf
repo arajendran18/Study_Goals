69c3fb9a26eb1bc827387280adfc4161140552e9
Ajithkumar R <168090251+arajendran18@users.noreply.github.com>
2024-08-03 18:28:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_instance" "Demo" {
-  ami           = "ami-0c2af51e265bd5e0e"
-  instance_type = "t2.micro"
-  key_name = "key_pair_name"
-  tags = {
-    Name = "Demo"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket  = "my-unique-bucket-name18961234"
+  tags    = {
+        Name          = "MyS3Bucket"
+        Environment    = "Production"
   }
 }
